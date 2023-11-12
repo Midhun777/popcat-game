68cfabd3a9bb967596583b8a0c3c51382b1aa8d0
midmat4828@gmail.com <midmat4828@gmail.com>
2023-11-12 14:56:40 +0530

    bug bug bug

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+window.onload = function() {
+	var img = document.getElementById("cat1");
+	var count = document.getElementById("score");
+	var score = 0;
+	var audio = new Audio("/media/pop.mp3");
+
+	img.addEventListener('mousedown', function() {
+		increaseScore();
+		img.src = '/media/cat2.png';
+		audio.play();
+
+	});
+
+	img.addEventListener('mouseup', function() {
+		img.src = '/media/cat1.png';
+		audio.play();
+	});
+
+	function increaseScore() {
+		score++;
+		count.innerHTML = score;
+	}
+}
