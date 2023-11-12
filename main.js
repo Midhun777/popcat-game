6afa1fdcd13782c64ba01778ea0ea6cb5802ec4a
midmat4828@gmail.com <midmat4828@gmail.com>
2023-11-12 19:53:55 +0530

    aaa uuuu eee ooo iiiii

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/* window.onload = function() {
 	var img = document.getElementById('cat1');
 	var count = document.getElementById("score");
 	var score = 0;
@@ -20,4 +20,35 @@ window.onload = function() {
 		score++;
 		count.innerHTML = score;
 	}
+} */
+
+var img = document.getElementById('cat1');
+var audio = new Audio("/media/pop.mp3");
+var count = document.getElementById("score");
+var score = 0;
+
+
+function change() {
+	img.src = "/media/cat2.png";
+	audio.play();
+	increaseScore();
+
+
 }
+
+function reset() {
+	img.src = "/media/cat1.png";
+	audio.play();
+}
+
+function imgreset() {
+	img.src = "/media/cat1.png";
+}
+
+function increaseScore() {
+	score++;
+	count.innerHTML = score;
+}
+
+
+setInterval(imgreset, 340);
